263397eefddb33494f94e7f347037108d9281882
thuankg1752 <thuankg1752@gmail.com>
2021-03-19 15:42:49 +0700

    update cau truc files

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+Route::get('/',  function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
@@ -28,4 +28,4 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
